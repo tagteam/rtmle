17017f86aa1d4a119bb2a987146a00013e2d7dc4
Thomas Alexander Gerds <tag@biostat.ku.dk>
2024-10-28 16:04:59 +0100

    now learning works with ranger and glmnet

---
+++ R/learn_ranger.R
@@ -0,0 +1,55 @@
+### learn_ranger.R --- 
+#----------------------------------------------------------------------
+## Author: Thomas Alexander Gerds
+## Created: Oct 28 2024 (09:26) 
+## Version: 
+## Last-Updated: Oct 28 2024 (16:03) 
+##           By: Thomas Alexander Gerds
+##     Update #: 18
+#----------------------------------------------------------------------
+## 
+### Commentary: 
+## 
+### Change Log:
+#----------------------------------------------------------------------
+## 
+### Code:
+learn_ranger <- function(character_formula,data,intervened_data,...){
+    # extract the data
+    # FIXME: should not let NA's pass until here
+    model_frame <- stats::model.frame(stats::formula(character_formula),
+                                      data = data,
+                                      drop.unused.levels = TRUE,
+                                      na.action = na.omit)
+    if (!inherits(try(
+             fit <- ranger::ranger(formula = stats::formula(character_formula),
+                                   data = model_frame,
+                                   probability = TRUE,
+                                   ...)
+            ,silent = TRUE),
+             "try-error")){
+    }else{
+        stop(paste0("\nCould not fit model with ranger:\n",
+                    "Formula:",character_formula))
+    }
+    
+    predicted_values <- vector(mode = "numeric",length = NROW(intervened_data))
+    this <- which(apply(intervened_data,1,function(x)any(is.na(x))))
+    if (any(this)){
+        predicted_values[this] <- as.numeric(NA)
+        if (inherits(try(    
+            predicted_values[!this] <- riskRegression::predictRisk(fit,newdata = intervened_data[!this])
+        ),"try-error")) stop("Ranger prediction failed")
+    }else{
+        if (inherits(try(    
+            predicted_values <- riskRegression::predictRisk(fit,newdata = intervened_data)
+        ),"try-error")) stop("Ranger prediction failed")
+    }
+    ## print(predicted_values)
+    data.table::setattr(predicted_values,"fit",NULL)
+    return(predicted_values)
+}
+
+
+######################################################################
+### learn_ranger.R ends here
---
+++ sandbox/test-rtmle-machine-learning.R
@@ -0,0 +1,45 @@
+### test-rtmle-machine-learning.R --- 
+#----------------------------------------------------------------------
+## Author: Thomas Alexander Gerds
+## Created: Oct 28 2024 (10:19) 
+## Version: 
+## Last-Updated: Oct 28 2024 (15:57) 
+##           By: Thomas Alexander Gerds
+##     Update #: 3
+#----------------------------------------------------------------------
+## 
+### Commentary: 
+## 
+### Change Log:
+#----------------------------------------------------------------------
+## 
+### Code:
+library(rtmle)
+library(data.table)
+library(targets)
+library(prodlim)
+tar_source("~/research/Methods/TMLE_for_breakfast/Ltmle/R/")
+source("~/research/Epidemi/Reddie/LEADER/functions/run_ltmle.R")
+source("~/research/Epidemi/Reddie/LEADER/functions/summary.runLtmle.R")
+set.seed(17)
+ld <- simulate_long_data(n = 11130,number_visits = 20,beta = list(A_on_Y = -.2,A0_on_Y = -0.3,A0_on_A = 6),register_format = TRUE)
+x <- rtmle_init(intervals = 3,name_id = "id",name_outcome = "Y",name_competing = "Dead",name_censoring = "Censored",censored_label = "censored")
+x$long_data <- ld[c("outcome_data","censored_data","competing_data","timevar_data")]
+add_baseline_data(x) <- ld$baseline_data[,start_followup_date:=0]
+x <- long_to_wide(x,intervals = seq(0,2000,30.45*12))
+protocol(x) <- list(name = "Always_A",treatment_variables = "A",intervention = 1)
+prepare_data(x) <- list()
+target(x) <- list(name = "Outcome_risk",strategy = "additive",estimator = "tmle",protocols = "Always_A")
+system.time(x <- run_rtmle(x,refit = TRUE,time_horizon = 2,learner = "learn_glm"))
+summary(x)
+system.time(x <- run_rtmle(x,refit = TRUE,time_horizon = 1:3,learner = "learn_glm"))
+summary(x)
+system.time(x <- run_rtmle(x,refit = TRUE,time_horizon = 1:3,learner = "learn_glmnet"))
+summary(x)
+system.time(x <- run_rtmle(x,refit = TRUE,time_horizon = 2,learner = "learn_ranger"))
+summary(x)
+system.time(x <- run_rtmle(x,refit = TRUE,time_horizon = 1:3,learner = "learn_ranger"))
+summary(x)
+
+######################################################################
+### test-rtmle-machine-learning.R ends here
