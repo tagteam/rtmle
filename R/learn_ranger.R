61b5e0e93f508abc4630e4bf15797a93c7b7349a
Thomas Alexander Gerds <tag@biostat.ku.dk>
2024-11-05 13:55:56 +0100

    fixed ranger

--- R/learn_ranger.R
+++ R/learn_ranger.R
@@ -3,9 +3,9 @@
 ## Author: Thomas Alexander Gerds
 ## Created: Oct 28 2024 (09:26) 
 ## Version: 
-## Last-Updated: Oct 29 2024 (08:13) 
+## Last-Updated: Nov  5 2024 (13:41) 
 ##           By: Thomas Alexander Gerds
-##     Update #: 22
+##     Update #: 41
 #----------------------------------------------------------------------
 ## 
 ### Commentary: 
@@ -21,10 +21,16 @@ learn_ranger <- function(character_formula,data,intervened_data,...){
                                       data = data,
                                       drop.unused.levels = TRUE,
                                       na.action = na.omit)
+    Y <- model_frame[[1]]
+    if (length(unique(Y)) == 2) probability <- TRUE else probability <- FALSE
+    if (probability) {
+        if (!is.factor(Y))
+            model_frame[[1]] <- factor(Y,levels = c(0,1))
+    }
     if (!inherits(try(
              fit <- ranger::ranger(formula = stats::formula(character_formula),
                                    data = model_frame,
-                                   probability = TRUE,
+                                   probability = probability,
                                    ...)
             ,silent = TRUE),
              "try-error")){
@@ -33,18 +39,21 @@ learn_ranger <- function(character_formula,data,intervened_data,...){
                     "Formula:",character_formula))
     }
     predicted_values <- vector(mode = "numeric",length = NROW(intervened_data))
-    this <- which(apply(intervened_data,1,function(x)any(is.na(x))))
-    if (any(this)){
-        predicted_values[this] <- as.numeric(NA)
-        if (inherits(try(    
-            predicted_values[!this] <- riskRegression::predictRisk(fit,newdata = intervened_data[!this])
-        ),"try-error")) stop("Ranger prediction failed")
+    no_missing <- !(apply(intervened_data,1,function(x)any(is.na(x))))
+    predicted_values[!no_missing] <- as.numeric(NA)
+    if (probability) {
+        if (inherits(try(
+            predicted_values[no_missing] <- riskRegression::predictRisk(fit,newdata = intervened_data[no_missing])
+        ),"try-error")) {
+            stop("Ranger prediction failed")
+        }
     }else{
-        if (inherits(try(    
-            predicted_values <- riskRegression::predictRisk(fit,newdata = intervened_data)
-        ),"try-error")) stop("Ranger prediction failed")
+        if (inherits(try(
+            predicted_values[no_missing] <- stats::predict(fit,data = intervened_data[no_missing],importance = "none")$predictions
+        ),"try-error")) {
+            stop("Ranger prediction failed")
+        }
     }
-    ## print(predicted_values)
     data.table::setattr(predicted_values,"fit",NULL)
     return(predicted_values)
 }
