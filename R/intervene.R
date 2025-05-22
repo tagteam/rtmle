ba987e27715893634fa07a45851b7cde0e8c505e
Alessandra Meddis <45624657+AMeddis@users.noreply.github.com>
2025-05-22 12:01:29 +0200

    time not needed

--- R/intervene.R
+++ R/intervene.R
@@ -1,30 +1,32 @@
-### intervene.R --- 
+### intervene.R ---
 #----------------------------------------------------------------------
 ## Author: Thomas Alexander Gerds
-## Created: Jul  3 2024 (13:46) 
-## Version: 
-## Last-Updated: Mar  7 2025 (18:41) 
+## Created: Jul  3 2024 (13:46)
+## Version:
+## Last-Updated: Nov  2 2024 (07:25)
 ##           By: Thomas Alexander Gerds
-##     Update #: 26
+##     Update #: 22
 #----------------------------------------------------------------------
-## 
-### Commentary: 
-## 
+##
+### Commentary:
+##
 ### Change Log:
 #----------------------------------------------------------------------
-## 
+##
 ### Code:
 intervene <- function(data,
-                      intervention_table,
-                      time){
+                      intervention_table){ ## why this is here??
+    ## FIXME: is it optional to set variables at all times or only the current and later?
+    ##        believe not but need to check Schnitzer et al.
     interdata <- copy(data)
-    N <- NROW(interdata)
     for (k in 1:nrow(intervention_table)){
         set(interdata,
             j = intervention_table[k][["variable"]],
-            value = rep(intervention_table[k][["value"]],N))
+            value = intervention_table[k][["value"]])
     }
     interdata
 }
 ######################################################################
 ### intervene.R ends here
+
+
