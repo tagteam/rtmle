67416047fed4ed0751925b69407254aa422fd769
Thomas Alexander Gerds <tag@biostat.ku.dk>
2026-03-13 13:51:07 +0100

    removed sim functions

--- tests/testthat/test-trans-int-data.R
+++
@@ -1,7 +0,0 @@
-library(testthat)
-test_that("trans-int-data transforms data correctly",{
-  set.seed(856)
-  data1 <- sim_op_data(50)
-  data2 <- trans_int_data(data1)
-  expect_true(nrow(data1) == nrow(data2))
-})
